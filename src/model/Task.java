20a073e436bbd2caee804f1d6cd06ff2feca282a
mallek <mallekr@3il.fr>
2024-04-05 13:54:58 +0200

    Configuration projet + ajout de l'entité Task

---
+++ src/model/Task.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Task {
+    private String title;
+    private String description;
+    private String dateEcheance;
+
+    public Task(String title, String description, String dateEcheance) {
+        this.title = title;
+        this.description = description;
+        this.dateEcheance = dateEcheance;
+    }
+}
